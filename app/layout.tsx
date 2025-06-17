5dfc2691de566136ace57ff04492de0922c921af
kuzowebsite <129853786+kuzowebsite@users.noreply.github.com>
2025-06-17 19:10:06 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'skydevs',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'skydevs',
 }
 
 export default function RootLayout({
